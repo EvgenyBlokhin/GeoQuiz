5fe9614344fb30ff6777a19a1036bb4e8edf2f97
EvgenyBlokhin <evgenyblokhin@mail.ru>
2018-02-03 01:34:51 +0700

    3/02/2018

--- app/src/main/java/ru/uj/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/uj/geoquiz/QuizActivity.java
@@ -1,13 +1,65 @@
 package ru.uj.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion();
     }
+
+    public void onClickButtonTrue(View view) {
+        checkAnswer(true);
+    }
+
+    public void onClickButtonFalse(View view) {
+        checkAnswer(false);
+    }
+
+    public void onClickNext(View view) {
+        mCurrentIndex = (mCurrentIndex + 1);
+        mCurrentIndex = mCurrentIndex % mQuestionBank.length;
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
 }
